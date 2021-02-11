777c5fde77c5b3157a456edf7c25a0baf94d93b3
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2021-02-11 19:23:09 +0530

    routes completed

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var mongoose = require('mongoose');
 
 const app = express();
 var url = "mongodb://127.0.0.1:27017/Bloggers";
-mongoose.connect(url);
+mongoose.connect(url,{ useNewUrlParser: true,useUnifiedTopology: true  });
 
 var db = mongoose.connection;
 db.on("connected",()=>{
-    
+    console.log("Connected");
+})
+
+db.on("error",()=>{
+    console.log("Not Connected");
 })
 
 
--- routes/blogs.js
+++ routes/blogs.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 router.get("/blogs",(req,res,next)=>{
     res.status(200).json({
-        message:"In /blogs route"
+        message:"In /blogs get route"
+    });
+});
+
+router.post("/blogs",(req,res,next)=>{
+    res.status(200).json({
+        message:"In /blogs post route"
+    });
+});
+
+router.patch("/blogs",(req,res,next)=>{
+    res.status(200).json({
+        message:"In /blogs patch route"
+    });
+});
+
+router.delete("/blogs",(req,res,next)=>{
+    res.status(200).json({
+        message:"In /blogs delete route"
     });
 });
 
